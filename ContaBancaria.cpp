4cb9b4d925e494abfc5d48c80674460c6cd3bd46
Fernando Cesar <cmnando.silva@gmail.com>
2020-10-23 02:03:24 -0300

    Programando as funções da Classe ContaBancaria

--- ContaBancaria.cpp
+++ ContaBancaria.cpp
@@ -1,5 +1,30 @@
 #include "ContaBancaria.hpp"
 
-ContaBancaria::ContaBancaria(string Senha){
+ContaBancaria::ContaBancaria(string Senha)
+{
     this->senha = Senha;
+}
+string ContaBancaria::getSenha()
+{
+    return this->senha;
+}
+int ContaBancaria::getNumero()
+{
+    return this->numero;
+}
+int ContaBancaria::getSaldo()
+{
+    return this->saldo;
+}
+void ContaBancaria::setSenha(string Senha)
+{
+    this->senha = Senha
+}
+void ContaBancaria::setNumero(int Numero)
+{
+    this->numero = Numero;
+}
+void ContaBancaria::setSaldo(int Saldo)
+{
+    this->saldo = Saldo;
 }
